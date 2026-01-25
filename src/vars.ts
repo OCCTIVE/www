a3b2b434bc6a28ab1cd7a2d787fabca8d79d98c1
Melvyn <42531205+melvyn9@users.noreply.github.com>
2026-01-24 22:45:12 -0800

    Workshop Page Toggle for Navbar (#38)

--- src/vars.ts
+++ src/vars.ts
@@ -1,7 +1,11 @@
+export const ENABLE_WORKSHOP = false;
+
 export const pages = [
   { title: 'Overview', link: '/' },
   { title: 'Library', link: '/library' },
   { title: 'About', link: '/about' },
   { title: 'Adopt OCCTIVE', link: '/adopt' },
-  { title: 'Workshop', link: '/workshop' },
+  ...(ENABLE_WORKSHOP
+    ? [{ title: 'Workshop', link: '/workshop' }]
+    : []),
 ];
