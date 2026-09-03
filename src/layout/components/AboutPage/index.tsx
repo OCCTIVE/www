99f100c7d28ead7df345f3250be80a97c4e610e6
Kristina <striegnk@union.edu>
2026-09-03 11:32:44 -0400

    fixed tabs -> spaces

--- src/layout/components/AboutPage/index.tsx
+++ src/layout/components/AboutPage/index.tsx
@@ -238,8 +238,8 @@ function AboutPage() {
           />
           <p>
             OCCTIVE is supported by the NSF grant &ldquo;The efficacy of a
-	    computing-concepts video library for students and peer tutors in
-	    multidisciplinary contexts&rdquo; (Award Numbers: 2337251, 2337252,
+            computing-concepts video library for students and peer tutors in
+            multidisciplinary contexts&rdquo; (Award Numbers: 2337251, 2337252,
             2337253, 2337254).
           </p>
         </div>
